6cbf69f1ba14b001195d1cd3876be035d30fb65f
Varshini S <varshini12cs@gmail.com>
2026-01-12 12:35:10 +0530

    Add pytthon test file

---
+++ .github/workflows/test.py
@@ -0,0 +1,11 @@
+def add(a,b):
+    return a+b
+x=5
+y=7
+result=add(x,y)
+print("Aaddition result is:", result)
+if result == 12:
+    print("Logic working correctly")
+else:
+    print("something went weong")
+    
